cfa614e277892afe738b94efc449446ab1f0be8e
espoupou <espoirativon@gmail.com>
2023-11-19 06:25:42 -0800

    partial task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import shlex
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        from models import storage
+        objs = storage.all()
+        cityl = []
+        result = []
+
+        for key in objs:
+            var = key.replace('.', ' ')
+            var = shlex.split(var)
+            if (var[0] == 'City'):
+                cityl.append(objs[key])
+
+        for elem in cityl:
+            if (elem.state_id == self.id):
+                result.append(elem)
+
+        return (result)
