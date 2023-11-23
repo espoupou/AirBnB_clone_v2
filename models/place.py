7de30c70b70765fcad5de795d03ba16f075370c8
espoupou <126259211+espoupou@users.noreply.github.com>
2023-11-23 19:24:30 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -42,7 +42,7 @@ class Place(BaseModel, Base):
         price_by_night = 0
         latitude = 0.0
         longitude = 0.0
-        amenity_ids = []
+        amenities = []
 
         @property
         def amenities(self):
