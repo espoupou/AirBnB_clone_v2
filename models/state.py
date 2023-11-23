baf1b0cb514b359e5c16940f29a14aeb05813b5e
espoupou <126259211+espoupou@users.noreply.github.com>
2023-11-23 01:44:59 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if models.is_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ""
+
+    if models.is_type != 'db':
+        @property
+        def cities(self):
+            cities_list = []
+            all_cities = models.storage.all(City).values()
+            for city in all_cities:
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
