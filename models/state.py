b17a7a25ebbfc60368a468bb9953790fe343d885
espoupou <126259211+espoupou@users.noreply.github.com>
2023-11-23 02:01:05 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -8,7 +8,7 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    if models.is_type == "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade='delete')
