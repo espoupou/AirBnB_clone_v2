bd6739ebd9ffec4d631ffb32ca2f5d039cc9cc83
espoupou <126259211+espoupou@users.noreply.github.com>
2023-11-23 03:13:57 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -9,12 +9,18 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship('Place', backref='user', cascade='delete')
-    reviews = relationship('Review', backref='user', cascade='delete')
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship('Place', backref='user', cascade='delete')
+        reviews = relationship('Review', backref='user', cascade='delete')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         @property
