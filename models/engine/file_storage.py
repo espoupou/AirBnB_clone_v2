a0e8521a9dcc0a2d429dce2962bcf6d401803c4d
espoupou <126259211+espoupou@users.noreply.github.com>
2023-11-23 03:27:43 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,7 +27,7 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({type(obj) + '.' + obj.id: obj})
+        self.all().update({str(type(obj)) + '.' + obj.id: obj})
         # self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
