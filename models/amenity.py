0b8a551ed7c9e90d6e7b8678d0c341a59bd38813
espoupou <espoirativon@gmail.com>
2023-11-23 07:53:02 -0800

    fix amenity import

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import getenv
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """amenties """
+    __tablename__ = "amenities"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity)
