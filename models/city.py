d04390c87505edfe574e3c3cd6695c7ba2741563
espoupou <126259211+espoupou@users.noreply.github.com>
2023-11-23 02:00:03 +0100

    Update city.py fix env

--- models/city.py
+++ models/city.py
@@ -2,11 +2,12 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    if models.is_type == "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
