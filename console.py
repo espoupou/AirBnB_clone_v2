e2350ddf177bf5fa277a9ba88dd8a22e9618e094
espoupou <espoirativon@gmail.com>
2023-11-22 16:15:49 -0800

    update

--- console.py
+++ console.py
@@ -119,13 +119,12 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not line:
                 raise SyntaxError()
-            print("test")
             my_list = line.split(" ")
             obj = eval("{}()".format(my_list[0]))
             print("{}".format(obj.id))
             for num in range(1, len(my_list)):
                 my_list[num] = my_list[num].replace('=', ' ')
-                attributes = split(my_list[num])
+                attributes = my_list[num].split(" ")
                 attributes[1] = attributes[1].replace('_', ' ')
                 try:
                     var = eval(attributes[1])
@@ -139,7 +138,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-            
+
         # if not args:
         #     print("** class name missing **")
         #     return
