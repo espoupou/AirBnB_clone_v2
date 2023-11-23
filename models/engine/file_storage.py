31286fe2bf072986b365d31da5a6b8912e515001
espoupou <126259211+espoupou@users.noreply.github.com>
2023-11-23 03:04:30 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,10 +43,10 @@ class FileStorage:
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
-        from models.state import State
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
+        from models.state import State
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
