7885e60681c550d627f4bf5954bdbffeb3afc0e2
espoupou <126259211+espoupou@users.noreply.github.com>
2023-12-10 19:25:00 +0100

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,27 +2,27 @@
 
 # Nginx configuration file
 $nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By ${hostname};
 
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-    server_name _;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+        root   /var/www/html;
+        index  index.html index.htm;
+        server_name _;
+        location /hbnb_static {
+                alias /data/web_static/current;
+                index index.html index.htm;
+        }
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
 
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
+        error_page 404 /404.html;
+        location /404 {
+                root /var/www/html;
+                internal;
+        }
 }"
 
 package { 'nginx':
