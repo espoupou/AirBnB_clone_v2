e5c8a9172138a07719cfc5028a0e144f67c7f0b0
espoupou <126259211+espoupou@users.noreply.github.com>
2023-12-10 19:37:48 +0100

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -28,27 +28,27 @@ $nginx_conf = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
+}
 
 file { '/data':
   ensure  => 'directory'
-} ->
+}
 
 file { '/data/web_static':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/shared':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
@@ -59,12 +59,12 @@ file { '/data/web_static/releases/test/index.html':
     Holberton School
   </body>
 </html>"
-} ->
+}
 
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
+}
 
 exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
@@ -72,26 +72,26 @@ exec { 'chown -R ubuntu:ubuntu /data/':
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
 file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Hello world!"
-} ->
+}
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-} ->
+}
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-} ->
+}
 
 exec { 'nginx restart':
   path => '/etc/init.d/'
