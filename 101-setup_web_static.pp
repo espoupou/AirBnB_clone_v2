c2a4c469ef6b3e4496e8bd44e715722f41b07b44
espoupou <126259211+espoupou@users.noreply.github.com>
2023-12-10 18:05:24 +0100

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -15,7 +15,7 @@ $nginx_conf = "server {
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
 
     error_page 404 /404.html;
